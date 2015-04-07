2e0b10a4458f465382fd4a9d02fe74238d37fd15
Jan Wagner <waja@cyconet.org>
2015-04-07 15:53:17 +0200

    Putting resultdir into variable

--- Dockerfiles/testssl.sh/contrib/testssl_cron.sh
+++ Dockerfiles/testssl.sh/contrib/testssl_cron.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 TEMPSCRIPT=/tmp/testssl.sh
+RESULTDIR=/srv/testssl
 
 # generating test script and running tests
 rm -rf ${TEMPSCRIPT};\
+echo "RESULTDIR=${RESULTDIR}" >> ${TEMPSCRIPT}
 for I in $(cat /etc/testssl.conf); do \
- echo $I| awk -F':' '{printf "docker run --rm -it waja/testssl.sh --starttls %s %s:%s | grep -v ':/usr/bin/openssl' | grep -v 'built:' | grep -v #Testing now# | grep -v #Done now# > /srv/testssl/%s_%s\n",$3,$1,$2,$1,$2}'>> ${TEMPSCRIPT}; \
+ echo $I| awk -F':' '{printf "docker run --rm -it waja/testssl.sh --starttls %s %s:%s | grep -v ':/usr/bin/openssl' | grep -v 'built:' | grep -v #Testing now# | grep -v #Done now# > ${RESULTDIR}/%s_%s\n",$3,$1,$2,$1,$2}'>> ${TEMPSCRIPT}; \
 done && \
 sed -i "s/#/'/g" ${TEMPSCRIPT}
 sh ${TEMPSCRIPT}
-cd /srv/testssl && git add -A && git commit -am "Changes from $(date +%s)"
+cd ${RESULTDIR} && git add -A && git commit -am "Changes from $(date +%s)"
