57efde7f40f134af58ecfff949d575ab456bc909
Jan Wagner <waja@cyconet.org>
2015-04-07 16:07:33 +0200

    Turning config filename into variable

--- Dockerfiles/testssl.sh/contrib/testssl_cron.sh
+++ Dockerfiles/testssl.sh/contrib/testssl_cron.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 TEMPSCRIPT=/tmp/testssl.sh
 RESULTDIR=/srv/testssl
+HOSTCONFIG=/etc/testssl.conf
 
 # removing old script
 rm -rf ${TEMPSCRIPT};\
 # generating test script
 echo "RESULTDIR=${RESULTDIR}" >> ${TEMPSCRIPT}
-for I in $(cat /etc/testssl.conf); do \
+for I in $(cat ${HOSTCONFIG}); do \
  echo $I| awk -F':' '{printf "docker run --rm -it waja/testssl.sh --starttls %s %s:%s | grep -v ':/usr/bin/openssl' | grep -v 'built:' | grep -v #Testing now# | grep -v #Done now# > ${RESULTDIR}/%s_%s\n",$3,$1,$2,$1,$2}'>> ${TEMPSCRIPT}; \
 done && \
 # Fixing our (intensionally) broken script
