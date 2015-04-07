def9a0556d0bab5c15c7329850643eed3e6ddcc3
Jan Wagner <waja@cyconet.org>
2015-04-07 15:42:27 +0200

    Putting tempscript into variable

--- Dockerfiles/testssl.sh/contrib/testssl_cron.sh
+++ Dockerfiles/testssl.sh/contrib/testssl_cron.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+TEMPSCRIPT=/tmp/testssl.sh
 
 # generating test script and running tests
-rm -rf /tmp/testssl.sh;\
+rm -rf ${TEMPSCRIPT};\
 for I in $(cat /etc/testssl.conf); do \
- echo $I| awk -F':' '{printf "docker run --rm -it waja/testssl.sh --starttls %s %s:%s | grep -v ':/usr/bin/openssl' | grep -v 'built:' | grep -v #Testing now# | grep -v #Done now# > /srv/testssl/%s_%s\n",$3,$1,$2,$1,$2}'>> /tmp/testssl.sh; \
+ echo $I| awk -F':' '{printf "docker run --rm -it waja/testssl.sh --starttls %s %s:%s | grep -v ':/usr/bin/openssl' | grep -v 'built:' | grep -v #Testing now# | grep -v #Done now# > /srv/testssl/%s_%s\n",$3,$1,$2,$1,$2}'>> ${TEMPSCRIPT}; \
 done && \
-sed -i "s/#/'/g" /tmp/testssl.sh
-sh /tmp/testssl.sh
+sed -i "s/#/'/g" ${TEMPSCRIPT}
+sh ${TEMPSCRIPT}
 cd /srv/testssl && git add -A && git commit -am "Changes from $(date +%s)"
