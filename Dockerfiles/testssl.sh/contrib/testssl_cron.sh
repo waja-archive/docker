d1b20a834837572556c9975326c926e48ed2a4fb
Jan Wagner <waja@cyconet.org>
2015-04-07 16:04:23 +0200

    Adding some comments

--- Dockerfiles/testssl.sh/contrib/testssl_cron.sh
+++ Dockerfiles/testssl.sh/contrib/testssl_cron.sh
@@ -2,12 +2,16 @@
 TEMPSCRIPT=/tmp/testssl.sh
 RESULTDIR=/srv/testssl
 
-# generating test script and running tests
+# removing old script
 rm -rf ${TEMPSCRIPT};\
+# generating test script
 echo "RESULTDIR=${RESULTDIR}" >> ${TEMPSCRIPT}
 for I in $(cat /etc/testssl.conf); do \
  echo $I| awk -F':' '{printf "docker run --rm -it waja/testssl.sh --starttls %s %s:%s | grep -v ':/usr/bin/openssl' | grep -v 'built:' | grep -v #Testing now# | grep -v #Done now# > ${RESULTDIR}/%s_%s\n",$3,$1,$2,$1,$2}'>> ${TEMPSCRIPT}; \
 done && \
+# Fixing our (intensionally) broken script
 sed -i "s/#/'/g" ${TEMPSCRIPT}
+# running test script
 sh ${TEMPSCRIPT}
+# commiting the (new) results with propper timestamp
 cd ${RESULTDIR} && git add -A && git commit -am "Changes from $(date +%s)"
