408da524168da4bb5c5d1c73d42653c41160d1dc
Jan Wagner <waja@cyconet.org>
2015-04-08 02:30:13 +0200

    Try detecting tty

--- Dockerfiles/testssl.sh/contrib/testssl_cron.sh
+++ Dockerfiles/testssl.sh/contrib/testssl_cron.sh
@@ -7,8 +7,9 @@ HOSTCONFIG=/etc/testssl.conf
 rm -rf ${TEMPSCRIPT};\
 # generating test script
 echo "RESULTDIR=${RESULTDIR}" >> ${TEMPSCRIPT}
+echo '[ "$(/usr/bin/tty || true)" != "not a tty" ] && DOCKER_RUN_OPTS="-i"' >> ${TEMPSCRIPT}
 for I in $(cat ${HOSTCONFIG}); do \
- echo $I| awk -F':' '{printf "docker run --rm -it waja/testssl.sh --starttls %s %s:%s | grep -v ':/usr/bin/openssl' | grep -v 'built:' | grep -v #Testing now# | grep -v #Done now# > ${RESULTDIR}/%s_%s\n",$3,$1,$2,$1,$2}'>> ${TEMPSCRIPT}; \
+ echo $I| awk -F':' '{printf "docker run --rm -t ${DOCKER_RUN_OPTS} waja/testssl.sh --starttls %s %s:%s | grep -v ':/usr/bin/openssl' | grep -v 'built:' | grep -v #Testing now# | grep -v #Done now# > ${RESULTDIR}/%s_%s\n",$3,$1,$2,$1,$2}'>> ${TEMPSCRIPT}; \
 done && \
 # Fixing our (intensionally) broken script
 sed -i "s/#/'/g" ${TEMPSCRIPT}
