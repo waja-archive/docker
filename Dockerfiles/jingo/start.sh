587cc770dd4f03070604fbb82dea00c4e9b5a8cc
Jan Wagner <waja@cyconet.org>
2016-02-07 14:15:27 +0100

    Use own config directory for easy volume mounting the config

--- Dockerfiles/jingo/start.sh
+++ Dockerfiles/jingo/start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+JINGO_CONFIG_PATH="${JINGO_CONFIG_PATH:-/opt/jingo/config/}"
 JINGO_APPLICATION_TITLE="${JINGO_APPLICATION_TITLE:-Jingo Wiki}"
 JINGO_REPOSITORY="${JINGO_REPOSITORY:-/opt/wiki-content}"
 JINGO_HOSTNAME="${JINGO_HOSTNAME:-localhost}"
@@ -17,14 +18,14 @@ if [ ! -d "$JINGO_REPOSITORY/.git" ]; then
   popd
 fi
 
-if [ ! -f /opt/jingo/config.yaml ]; then
-  /opt/jingo/jingo -s > /opt/jingo/config.yaml
+if [ ! -f "$JINGO_CONFIG_PATH/config.yaml" ]; then
+  /opt/jingo/jingo -s > "$JINGO_CONFIG_PATH/config.yaml"
 fi
 
-# sed -ri "s#^  title:.*#  title: $JINGO_APPLICATION_TITLE#" /opt/jingo/config.yaml
-# sed -ri "s#^  repository:.*#  repository: \'$JINGO_REPOSITORY\'#" /opt/jingo/config.yaml
-# sed -ri "s#^  hostname:.*#  hostname: $JINGO_HOSTNAME#" /opt/jingo/config.yaml
-# sed -ri "s#^  port:.*#  port: $JINGO_POST#" /opt/jingo/config.yaml
+# sed -ri "s#^  title:.*#  title: $JINGO_APPLICATION_TITLE#" "$JINGO_CONFIG_PATH/config.yaml"
+# sed -ri "s#^  repository:.*#  repository: \'$JINGO_REPOSITORY\'#" "$JINGO_CONFIG_PATH/config.yaml"
+# sed -ri "s#^  hostname:.*#  hostname: $JINGO_HOSTNAME#" "$JINGO_CONFIG_PATH/config.yaml"
+# sed -ri "s#^  port:.*#  port: $JINGO_POST#" "$JINGO_CONFIG_PATH/config.yaml"
 
 cd /opt/jingo
 npm start
